1b22319b5d45489d87d62f3b7e1acec2d55c5597
Shashwat Sahai <ssahai97@gmail.com>
2021-09-25 12:49:56 +0530

    changes

--- public/js/handler.js
+++ public/js/handler.js
@@ -1,6 +1,6 @@
 
  let turn = 0;
-const socket = io.connect('http://'+window.location.host);
+const socket = io.connect('https://'+window.location.host);
 
 
 function onDragMove(newLocation,oldLocation,source,piece,position,orientation) {
